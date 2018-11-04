c5541bfd607276db2666cbd7b502b69d8ea8a123
jyp930 <jyp930@snu.ac.kr>
2018-11-04 14:54:34 +0900

    Main & arbeit create, edit

--- TaDa/frontend/src/app/app.module.ts
+++ TaDa/frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ArbeitPostCreateComponent } from './arbeit-post-create/arbeit-post-create.component';
+import { ArbeitPostEditComponent } from './arbeit-post-edit/arbeit-post-edit.component';
+import { FormsModule } from "@angular/forms";
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArbeitPostCreateComponent,
+    ArbeitPostEditComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TaDa/frontend/src/app/arbeit-post-edit/arbeit-post-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arbeit-post-edit',
+  templateUrl: './arbeit-post-edit.component.html',
+  styleUrls: ['./arbeit-post-edit.component.css']
+})
+export class ArbeitPostEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
