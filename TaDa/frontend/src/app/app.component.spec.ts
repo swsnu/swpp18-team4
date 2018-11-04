cdab479b518635e1e437c54682fad1474f816cc4
jyp930 <jyp930@snu.ac.kr>
2018-11-04 16:43:19 +0900

    arbeit create without css

--- TaDa/frontend/src/app/app.component.spec.ts
+++ TaDa/frontend/src/app/app.component.spec.ts
@@ -1,27 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterOutletStubComponent,
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'TaDa'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TaDa');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TaDa!');
   }));
 });
--- TaDa/frontend/src/app/arbeit-post-create/arbeit-post-create.component.spec.ts
+++ TaDa/frontend/src/app/arbeit-post-create/arbeit-post-create.component.spec.ts
@@ -2,13 +2,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArbeitPostCreateComponent } from './arbeit-post-create.component';
 
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from "@angular/forms";
+
 describe('ArbeitPostCreateComponent', () => {
   let component: ArbeitPostCreateComponent;
   let fixture: ComponentFixture<ArbeitPostCreateComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ArbeitPostCreateComponent ]
+      declarations: [ ArbeitPostCreateComponent ],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +24,79 @@ describe('ArbeitPostCreateComponent', () => {
     fixture = TestBed.createComponent(ArbeitPostCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.ngOnInit();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('test isValiddInput func', () => {
+    let region = component.region;
+    expect(component.isValidInput(region)).toBeFalsy();
+    region = '';
+    expect(component.isValidInput(region)).toBeFalsy();
+    region = 'Nakdae';
+    expect(component.isValidInput(region)).toBeTruthy();
+  });
+
+  it('test view_write_tab func', () => {
+    component.view_write_tab();
+    expect(component.view_write).toEqual(true);
+    expect(document.getElementById('write-tab-button').getAttribute('class')).toContain('Clicked');
+    expect(document.getElementById('preview-tab-button').getAttribute('class')).toContain('unClicked');
+  });
+
+  it('test view_preview_tab func', () => {
+    component.view_preview_tab();
+    expect(component.view_write).toEqual(false);
+    expect(document.getElementById('write-tab-button').getAttribute('class')).toContain('unClicked');
+    expect(document.getElementById('preview-tab-button').getAttribute('class')).toContain('Clicked');
+  });
+
+  it('test back func when yes', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    component.back();
+    /* url test */
+  });
+
+  it('test back func when no', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    component.back();
+    /* url test */
+  });
+
+  it('test confirm func', () => {
+    spyOn(window, "alert");
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("지역을 입력해 주세요.");
+    component.region = 'Nakdae';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("아르바이트 종류를 입력해 주세요.");
+    component.arbeitType = 'Mentoring';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("시급을 입력해 주세요.");
+    component.pay = '5000';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("제목을 입력해 주세요.");
+    component.title = '제목입니다';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("내용을 입력해 주세요.");
+    component.content = '내용입니다';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("담당자 이름을 입력해 주세요.");
+    component.managerName = 'Team4';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("담당자 전화번호를 입력해 주세요.");
+    component.managerPhone = '010-0000-0000';
+    component.confirm();
+    expect(window.alert).toHaveBeenCalledWith("시간대를 입력해 주세요.");
+    component.timezones = '1111';
+    component.confirm();
+    expect(window.alert).toBeTruthy();
+
+  });
+
+
+
 });
