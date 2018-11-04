edd5aeb308cc8302196b2c310dc697416bc7a94f
jyp930 <jyp930@snu.ac.kr>
2018-11-04 12:45:53 +0900

    make backend, frontend folder

--- TaDa/frontend/src/app/app.module.ts
+++ TaDa/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ArbeitPostCreateComponent } from './arbeit-post-create/arbeit-post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArbeitPostCreateComponent
   ],
   imports: [
     BrowserModule,
