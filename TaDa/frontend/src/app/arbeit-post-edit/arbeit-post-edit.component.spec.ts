a63a855cbeadfa1c024bf7578cf57424b7477df8
jyp930 <jyp930@snu.ac.kr>
2018-11-04 21:49:34 +0900

    remove arbeit edit

--- TaDa/frontend/src/app/arbeit-post-edit/arbeit-post-edit.component.spec.ts
+++ TaDa/frontend/src/app/arbeit-post-edit/arbeit-post-edit.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArbeitPostEditComponent } from './arbeit-post-edit.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ArbeitPostEditComponent', () => {
   let component: ArbeitPostEditComponent;
@@ -8,7 +10,11 @@ describe('ArbeitPostEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ArbeitPostEditComponent ]
+      declarations: [ ArbeitPostEditComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ]
     })
     .compileComponents();
   }));
--- TaDa/frontend/src/app/log-in-user.service.spec.ts
+++ TaDa/frontend/src/app/log-in-user.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { User } from "./User";
 import { LoginUserService } from './log-in-user.service';
 
 describe('LogInUserService', () => {
@@ -8,5 +8,16 @@ describe('LogInUserService', () => {
   it('should be created', () => {
     const service: LoginUserService = TestBed.get(LoginUserService);
     expect(service).toBeTruthy();
+  });
+
+  it('test isLogedIn func', () => {
+    const service: LoginUserService = TestBed.get(LoginUserService);
+    expect(service.isLogedIn()).toBeFalsy();
+    service.LogInUser = new User;
+    expect(service.isLogedIn()).toBeTruthy();
+  });
+
+  it('test  getUserType func', () => {
+    
   });
 });
