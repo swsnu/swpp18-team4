382646dce97ce4c009baadf4e7dfa91e943c69de
jyp930 <jyp930@snu.ac.kr>
2018-11-04 22:23:32 +0900

    loginuserservice add

--- TaDa/frontend/src/app/log-in-user.service.spec.ts
+++ TaDa/frontend/src/app/log-in-user.service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { User } from "./User";
-import { LoginUserService } from './log-in-user.service';
+import {TestBed} from '@angular/core/testing';
+import {User} from "./User";
+import {LoginUserService} from './log-in-user.service';
+import {UserTypeEnum} from "./Enums/UserTypeEnum";
 
 describe('LogInUserService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -18,6 +19,18 @@ describe('LogInUserService', () => {
   });
 
   it('test  getUserType func', () => {
-    
+    const service: LoginUserService = TestBed.get(LoginUserService);
+    expect(service.getUserType()).toEqual('');
+    service.LogInUser = new User;
+    service.LogInUser.type = UserTypeEnum.Employee;
+    expect(service.getUserType()).toEqual('Employee');
+    service.LogInUser.type = UserTypeEnum.Employer;
+    expect(service.getUserType()).toEqual('Employer');
+
+  });
+
+  it('test signOut func', () => {
+    const service: LoginUserService = TestBed.get(LoginUserService);
+    service.signOut();
   });
 });
