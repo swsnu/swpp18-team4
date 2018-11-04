8d312a002d4000c4623f153cd65c3a935dabda52
Sangsoo Park <sangsoo@bagsangsuui-MacBook-Air.local>
2018-11-04 16:22:50 +0900

    add title component

--- TaDa/frontend/src/app/log-in-user.service.ts
+++ TaDa/frontend/src/app/log-in-user.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginUserService {
-
   constructor() { }
 
   isValidUser() {}
---
+++ TaDa/frontend/src/app/title/title.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
