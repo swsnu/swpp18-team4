b4bc2fd023894b45283e7bdf838b792210b00fb9
jyp930 <jyp930@snu.ac.kr>
2018-11-13 04:58:28 +0900

    add func Search By Keyword in arbeit bulletin

--- TaDa/frontend/src/app/arbeit-bulletin/arbeit-bulletin.component.ts
+++ TaDa/frontend/src/app/arbeit-bulletin/arbeit-bulletin.component.ts
@@ -10,6 +10,11 @@ import { mockArbeitPost } from '../mock-ArbeitPost';
 export class ArbeitBulletinComponent implements OnInit {
   mockData: ArbeitPost[]; // array to store all arbeit data.
   dataToShow: ArbeitPost[]; // only array data in dataToShow is shown in bulletin board.
+
+  /*variable for keyword searching */
+  search_keyword: string;
+  search_criteria: string;
+
   constructor() { }
 
   ngOnInit() {
@@ -18,5 +23,49 @@ export class ArbeitBulletinComponent implements OnInit {
   }
   getAuthorNameByID(id: number): string {
     return '아기상어';
+  }
+  search(): void {
+    this.dataToShow = this.mockData; // initialize dataToShow
+    /* do filtering */
+
+
+
+    /* do keyword searching*/
+    if(typeof this.search_criteria === 'undefined' ||
+        typeof this.search_keyword === 'undefined' ) {
+          return ;
+    } else {
+        this.search_keyword = this.search_keyword.trim();
+        if(this.search_keyword === ''){
+          return ;
+        } else {
+          this.doKeywordSearch();
+        }
+    }
+  }
+  doKeywordSearch(): void {
+    if(this.dataToShow) {
+      let temp_data: ArbeitPost[] = [];
+      if(this.search_criteria === '제목') {
+        for (const post of this.dataToShow) {
+          if(post.title.includes(this.search_keyword)) {
+            temp_data.push(post);
+          }
+        }
+      } else if(this.search_criteria === '내용') {
+        for (const post of this.dataToShow) {
+          if(post.content.includes(this.search_keyword)) {
+            temp_data.push(post);
+          }
+        }
+      } else {
+        for (const post of this.dataToShow) {
+          if(post.content.includes(this.search_keyword) || post.title.includes(this.search_keyword)) {
+            temp_data.push(post);
+          }
+        }
+      }
+      this.dataToShow = temp_data;
+    }
   }
 }
