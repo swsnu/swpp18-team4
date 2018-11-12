38d528935b11874e3c0ff206bdcab079a472e568
jyp930 <jyp930@snu.ac.kr>
2018-11-13 04:04:08 +0900

    separate variable in arbeit bulletin

--- TaDa/frontend/src/app/arbeit-bulletin/arbeit-bulletin.component.ts
+++ TaDa/frontend/src/app/arbeit-bulletin/arbeit-bulletin.component.ts
@@ -8,11 +8,13 @@ import { mockArbeitPost } from '../mock-ArbeitPost';
   styleUrls: ['./arbeit-bulletin.component.css']
 })
 export class ArbeitBulletinComponent implements OnInit {
-  mockData: ArbeitPost[];
+  mockData: ArbeitPost[]; // array to store all arbeit data.
+  dataToShow: ArbeitPost[]; // only array data in dataToShow is shown in bulletin board.
   constructor() { }
 
   ngOnInit() {
     this.mockData = mockArbeitPost;
+    this.dataToShow = this.mockData;
   }
   getAuthorNameByID(id: number): string {
     return '아기상어';
