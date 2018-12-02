19c38fa47ef53575c4810f3299fbb06fcd9dd5e6
Sangsoo Park <sangsoo@bagsangsuui-MacBook-Air.local>
2018-12-03 01:31:43 +0900

    fix angular testing error

--- TaDa/frontend/src/app/shared/nav/nav.component.spec.ts
+++ TaDa/frontend/src/app/shared/nav/nav.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -8,7 +10,11 @@ describe('NavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
