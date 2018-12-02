a4764b3b450e419f768d824a3ec735dccada7837
jyp930 <jyp930@snu.ac.kr>
2018-12-03 02:38:35 +0900

    more clear

--- TaDa/frontend/src/app/core/home/home.component.ts
+++ TaDa/frontend/src/app/core/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../models/user';
-import {UserService} from '../services/user.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,13 +7,7 @@ import {UserService} from '../services/user.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  connect: User;
-  constructor(
-    private userService: UserService,
-  ) { }
-  ngOnInit() {
-    this.userService.getUser(1)
-      .then(user => this.connect = user);
-  }
+  constructor() { }
+  ngOnInit() {}
 
 }
