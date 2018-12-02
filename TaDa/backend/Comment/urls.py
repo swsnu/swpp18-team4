898502d89dc08688fe5edab0cb4d4e556abb0c02
jyp930 <jyp930@snu.ac.kr>
2018-12-03 05:30:15 +0900

    edit url

--- TaDa/backend/Comment/urls.py
+++ TaDa/backend/Comment/urls.py
@@ -3,10 +3,10 @@ from Comment import views
 
 urlpatterns = [
     path('', views.comments, name='comments'),
-    path('<int:comment_id>', views.comment, name='comment'),
-    path('post/<int:post_id>', views.commentByPost, name='commentByPost'),
-    path('author/<int:author_id>', views.commentByAuthor, name='commentByAuthor'),
-    path('receive/<int:author_id>', views.commentReceive, name='commentReceive'),
-    path('token', views.token, name='token'),
+    path('<int:comment_id>/', views.comment, name='comment'),
+    path('post/<int:post_id>/', views.commentByPost, name='commentByPost'),
+    path('author/<int:author_id>/', views.commentByAuthor, name='commentByAuthor'),
+    path('receive/<int:author_id>/', views.commentReceive, name='commentReceive'),
+    path('token/', views.token, name='token'),
 ]
 
