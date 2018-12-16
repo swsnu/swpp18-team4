e021ad5ea208471c494426265394c30e94a8b6b5
jyp930 <jyp930@snu.ac.kr>
2018-12-16 23:50:07 +0900

    addgit add .

--- TaDa/frontend/src/app/post/components/post-view/post-view.component.ts
+++ TaDa/frontend/src/app/post/components/post-view/post-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../../core/services/post.service';
+import { UserService } from '../../../core/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import {Post} from '../../../core/models/post';
 
 @Component({
   selector: 'app-post-view',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-view.component.css']
 })
 export class PostViewComponent implements OnInit {
+  current_post: Post;
 
-  constructor() { }
+  constructor(
+    private post_service: PostService,
+    private user_service: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.post_service.getPostByPostId(id)
+      .then( post => this.current_post = post)
+      .catch( () => this.router.navigateByUrl('/post/list'));
   }
-
 }
