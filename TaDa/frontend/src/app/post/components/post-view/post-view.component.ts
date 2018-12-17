6f2da86669564906d581550d51fe49a289e67947
jyp930 <jyp930@snu.ac.kr>
2018-12-17 23:18:33 +0900

    detail-page map&company&comment

--- TaDa/frontend/src/app/post/components/post-view/post-view.component.ts
+++ TaDa/frontend/src/app/post/components/post-view/post-view.component.ts
@@ -20,6 +20,10 @@ import {Comment} from '../../../core/models/comment';
 export class PostViewComponent implements OnInit {
   current_post: Post;
   post_author: User;
+  post_comments: Comment[];
+  current_user: User;
+  id: number;
+  comment_value: string;
 
   constructor(
     private post_service: PostService,
@@ -31,12 +35,15 @@ export class PostViewComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.post_service.getPostByPostId(id)
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.post_service.getPostByPostId(this.id)
       .then( post => this.current_post = post)
       .then( () => this.user_service.getUser(this.current_post.author_id)
         .then( user => this.post_author = user))
       .catch( () => this.router.navigateByUrl('/post/list'));
+    this.comment_service.getWriteCommentsByPostId(this.id)
+      .then(comments => this.post_comments = comments);
+    this.current_user = this.user_service.getCurrentUser();
   }
 
   back(): void {
@@ -46,25 +53,36 @@ export class PostViewComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.router.navigateByUrl(`/post/edit/${id}`);
   }
-  ConfirmComment() {
-    const mock_comment: Comment = {
+  ConfirmComment(content, star, refer) {
+    const new_comment: Comment = {
       id: -1,
-      post_id: 49,
-      refer_comment_id: -1,
-      author_id: null,
-      star: 1,
-      content: 'content3',
+      post_id: this.id,
+      refer_comment_id: refer,
+      author_id: this.current_user.id,
+      star: star,
+      content: content,
       register_date: null,
       last_modify_date: null
     };
+    this.comment_service.createComment(new_comment)
+      .then( () => this.comment_service.getWriteCommentsByPostId(this.id)
+        .then(comments => this.post_comments = comments))
+      .then( () => this.comment_value = '' )
+      .catch( () => alert('에러!') );
+  }
 
-    this.comment_service.createComment(mock_comment)
-      .then( comment => console.log(comment) )
-      .catch( () => console.log('에러!') );
+  editComment(comment) {
+    this.toastrService.success('good');
+    this.comment_service.getWriteCommentsByPostId(this.id)
+      .then(comments => this.post_comments = comments)
+      .catch( () => alert('에러!') );
   }
-  getComment() {
-    this.comment_service.getReceiveCommentsByUserId(22)
-      .then( comment => console.log(comment))
-      .catch( () => console.log('에러2!') );
+  deleteComment(comment) {
+    this.comment_service.deleteComment(comment)
+      .then( () => this.comment_service.getWriteCommentsByPostId(this.id)
+        .then(comments => this.post_comments = comments))
+      .catch( () => alert('에러!') );
   }
+
+
 }
