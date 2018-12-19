c38bdd1ee66ca24102a31c5b675ccd62fd8e7b6f
cki86201 <cki86201@naver.com>
2018-12-20 05:29:30 +0900

    delete footer

--- TaDa/frontend/src/app/app.module.ts
+++ TaDa/frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FooterComponent } from './shared/footer/footer.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { SigninComponent } from './auth/components/signin/signin.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
@@ -26,7 +25,6 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent,
     NavComponent,
     SigninComponent,
     SignupComponent,
